3934c64cb63b0d12450ac76a52453d5234d82365
thebards <swordbookstory@gmail.com>
2023-09-01 14:45:29 +0000

    fix bugs

--- libs/langchain/langchain/agents/agent_toolkits/openapi/planner.py
+++ libs/langchain/langchain/agents/agent_toolkits/openapi/planner.py
@@ -318,7 +318,7 @@ def _create_api_controller_tool(
     base_url = api_spec.servers[0]["url"]  # TODO: do better.
 
     def _create_and_run_api_controller_agent(plan_str: str) -> str:
-        pattern = r"\b(GET|POST|PATCH|DELETE)\s+(/\S+)*"
+        pattern = r"\b(GET|POST|PATCH|DELETE)\s+(/\S*)*"
         matches = re.findall(pattern, plan_str)
         endpoint_names = [
             "{method} {route}".format(method=method, route=route.split("?")[0])
@@ -336,7 +336,7 @@ def _create_api_controller_tool(
         return agent.run(plan_str)
     
     async def _acreate_and_run_api_controller_agent(plan_str: str) -> str:
-        pattern = r"\b(GET|POST|PATCH|DELETE)\s+(/\S+)*"
+        pattern = r"\b(GET|POST|PATCH|DELETE)\s+(/\S*)*"
         matches = re.findall(pattern, plan_str)
         endpoint_names = [
             "{method} {route}".format(method=method, route=route.split("?")[0])
