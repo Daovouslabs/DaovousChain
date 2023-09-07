a34901d68ec229c6b6ef665fa9645aeea40b90a8
thebards <swordbookstory@gmail.com>
2023-09-07 12:33:02 +0000

    fix bugs

--- libs/langchain/langchain/json_utils/fix_json.py
+++ libs/langchain/langchain/json_utils/fix_json.py
@@ -6,8 +6,7 @@ def maybe_fix_json(text):
 	try:
 		text = text.strip()
 		text = re.sub(",+\s*}$", "}", text)
-		action_match1 = re.search(r"```(.*?)```?", text, re.DOTALL) or re.search(r"```json(.*?)```?", text, re.DOTALL) or re.search(r"```(.*?)$", text, re.DOTALL)
-		# match {*}
+		action_match1 = re.search(r"```json(.*?)```?", text, re.DOTALL) or re.search(r"```(.*?)```?", text, re.DOTALL) or re.search(r"```(.*?)$", text, re.DOTALL)
 		action_match2 = re.search(r"\{(.*?)\}", text, re.DOTALL) or re.search(r"\{(.*?)$", text, re.DOTALL)
 		if action_match1 is not None:
 			json_text = action_match1.group(1).strip()
@@ -19,7 +18,7 @@ def maybe_fix_json(text):
 		if json_text is not None:
 			if json_text[-1] != "}":
 				json_text += "}"
-			json_text = re.sub(",+\s*}$", "}", text)
+			json_text = re.sub(",+\s*}$", "}", json_text)
 			try:
 				data = json.loads(json_text)
 			except json.JSONDecodeError as e:
